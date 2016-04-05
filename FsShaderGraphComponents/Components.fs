104aa5c573627cd24f158178ef2ee0faa8843045
Nathan Letwory <nathan@mcneel.com>
2016-04-05 13:57:22 +0300

    Add Noise texture node.

--- FsShaderGraphComponents/Components.fs
+++ FsShaderGraphComponents/Components.fs
@@ -579,6 +579,35 @@ type EmissionBsdf() =
       let x = Utils.GetInputsXml inputs
       Utils.GetNodeXml node nickname x
 
+type NoiseTextureNode() =
+  inherit GH_Component("Noise", "noise", "Noise", "Shader", "Texture")
+
+  override u.RegisterInputParams(mgr : GH_Component.GH_InputParamManager) =
+    mgr.AddVectorParameter("Vector", "V", "Vector", GH_ParamAccess.item, Vector3d.Zero) |> ignore
+    mgr.AddNumberParameter("Scale", "S", "Scale", GH_ParamAccess.item, 5.0) |> ignore
+    mgr.AddNumberParameter("Detail", "D", "Detail", GH_ParamAccess.item, 2.0) |> ignore
+    mgr.AddNumberParameter("Distortion", "Dist", "Distortion", GH_ParamAccess.item, 0.0) |> ignore
+
+  override u.RegisterOutputParams(mgr : GH_Component.GH_OutputParamManager) =
+    mgr.AddColourParameter("Color", "C", "Color", GH_ParamAccess.item) |> ignore
+    mgr.AddNumberParameter("Fac", "F", "Fac", GH_ParamAccess.item) |> ignore
+
+  override u.ComponentGuid = new Guid("c3632808-8f29-48bd-afc2-0b85ad5763c0")
+
+  override u.Icon = Icons.Emission
+
+  override u.SolveInstance(DA: IGH_DataAccess) =
+    u.Message <- ""
+
+    DA.SetData(0, new GH_Colour(Color.Beige)) |> ignore
+    DA.SetData(1, 0.5) |> ignore
+
+  interface ICyclesNode with
+    member u.NodeName = "noise_texture"
+    member u.GetXml node nickname inputs =
+      let x = Utils.GetInputsXml inputs
+      Utils.GetNodeXml node nickname x
+
 type LayerWeightNode() =
   inherit GH_Component("Layer Weight", "layer weight", "Layer weight", "Shader", "Input")
 
