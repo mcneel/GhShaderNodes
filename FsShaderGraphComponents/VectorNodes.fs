8870db72d6039424e51e14651ccaeb927a448829
Nathan Letwory <nathan@mcneel.com>
2016-12-02 13:14:03 +0200

    Add vector nodes.

---
+++ FsShaderGraphComponents/VectorNodes.fs
@@ -0,0 +1,18 @@
+﻿namespace TextureNodes
+
+open FsShaderGraphComponents
+
+open System
+open System.Windows.Forms
+
+open Grasshopper.Kernel
+
+open ShaderGraphResources
+
+type BumpNode() =
+  inherit CyclesNode(
+    "Bump", "bump",
+    "Bump node",
+    "Shader", "Vector",
+    typeof<ccl.ShaderNodes.BumpNode>)
+  override u.ComponentGuid = u |> ignore; new Guid("02f39fc2-757a-4ec0-91f6-1822ca48663d")
