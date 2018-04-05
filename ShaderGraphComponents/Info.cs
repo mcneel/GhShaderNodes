57c7509054c7d94574eddd8315024a05ab24ce55
Nathan Letwory <nathan@mcneel.com>
2018-04-05 09:20:49 +0300

    Clean up unnecessary code. Bump version.

--- ShaderGraphComponents/Info.cs
+++ ShaderGraphComponents/Info.cs
@@ -10,50 +10,7 @@ namespace ShaderGraphComponents
 	{
 		public Info()
 		{
-			Instances.ComponentServer.GHAFileLoaded += ComponentServer_GHAFileLoaded;
 		}
-
-		private void ComponentServer_GHAFileLoaded(object sender, GH_GHALoadingEventArgs e)
-		{
-			if (!e.Id.Equals(Id)) return;
-
-			foreach(var et in e.Assembly.ExportedTypes)
-			{
-				try
-				{
-					if (Activator.CreateInstance(et) is object i)
-					{
-					}
-				} catch (Exception) { }
-			}
-		}
-
-		//let loaded = Instances.ComponentServer.GHAFileLoaded
-
-		//let cb (x:GH_GHALoadingEventArgs) =
-		//  if x.Id.Equals(it.Id) then
-		//    printfn "Match: %A %A" System.DateTime.Now x
-		//    printfn "%A" x.FileName
-		//    x.Assembly.ExportedTypes
-		//    |> Seq.filter (
-		//        fun et ->
-		//            try
-		//              let i = Activator.CreateInstance(et) :?> CyclesNode
-		//              true
-		//            with
-		//              | x -> false
-		//      )
-		//    |> Seq.map (fun t ->
-
-		//                  let i = Activator.CreateInstance(t) :?> CyclesNode
-		//                  i.ShaderNode.GetType(), i.ComponentGuid
-		//                )
-		//    |> Seq.iter (fun (k, v) ->
-		//                  Utils.node_componentmapping.Add(k, v)
-		//                  printfn "mapped n %A guid %A" k v)
-
-		//do
-		//  loaded |> Observable.subscribe cb |> ignore
 
 		public override string Name => "Shader Nodes";
 		public override string Description => "Create shader graphs for Cycles for Rhino";
